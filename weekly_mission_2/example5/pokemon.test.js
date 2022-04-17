f7af69e6bdf7b6bc47db1e753ae1a725907bff7e
Ajelandro19 <almon190699@gmail.com>
2022-04-17 16:59:26 -0500

    comentarios sobre pruebas unitarias

--- weekly_mission_2/example5/pokemon.test.js
+++ weekly_mission_2/example5/pokemon.test.js
@@ -4,3 +4,9 @@ test('1) Create a new object pokemon', () => {
   const myPokemon = new Pokemon('Pikachu')
   expect(myPokemon.name).toBe('Pikachu'); // Corrige esta prueba
 });
+
+
+///TDD test driven development
+//1) Escribir una prueba de lo que quieres modelar
+//2) correr la prueba y verla fallar
+// 3) Escribir el código que haga pasar la prueba
